6ee57c1b0113130a14d4246aa50e438d4df46bf3
Weronika <werawera112@gmail.com>
2020-12-03 14:01:54 +0100

    Updates

---
+++ precache-manifest.89366a6dc153a822857bede9c18331b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f682d05c78b0043d620b136c2eebf61",
+    "url": "/todo-multi-platform/index.html"
+  },
+  {
+    "revision": "969223c03339a8c61279",
+    "url": "/todo-multi-platform/static/css/main.40ecfed7.chunk.css"
+  },
+  {
+    "revision": "80f65f34cf0d59d6dff1",
+    "url": "/todo-multi-platform/static/js/2.d27372ef.chunk.js"
+  },
+  {
+    "revision": "969223c03339a8c61279",
+    "url": "/todo-multi-platform/static/js/main.7ab68dbb.chunk.js"
+  },
+  {
+    "revision": "8f132f7723e209239a44",
+    "url": "/todo-multi-platform/static/js/runtime-main.f7f440ee.js"
+  },
+  {
+    "revision": "0aa9e68da47936defc57441eca06119c",
+    "url": "/todo-multi-platform/static/media/img1.0aa9e68d.jpg"
+  },
+  {
+    "revision": "f8c220cbcf66154b7ddfe604561601c5",
+    "url": "/todo-multi-platform/static/media/img2.f8c220cb.jpg"
+  },
+  {
+    "revision": "46733691e71ef50b60923660378310d5",
+    "url": "/todo-multi-platform/static/media/img3.46733691.jpg"
+  },
+  {
+    "revision": "1fb6285aa6d072048dbd54522ae3ad86",
+    "url": "/todo-multi-platform/static/media/landscape-1.1fb6285a.jpg"
+  }
+]);
